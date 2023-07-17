0b3187191311228a84031d98a0dc0aa874587f1b
Valenthy24rg <110358760+Valenthy24rg@users.noreply.github.com>
2023-07-17 00:46:49 -0500

    2023_07_15_162719_create_departamento_table.php

--- database/migrations/2023_07_15_162719_create_departamento_table.php
+++ database/migrations/2023_07_15_162719_create_departamento_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('departamento', function (Blueprint $table) {
             $table->id();
+
+            $table->string('nombre', 50);
+
             $table->timestamps();
         });
     }
