179aa988183347b65dce34edc9df82165679652d
Valenthy24rg <110358760+Valenthy24rg@users.noreply.github.com>
2023-07-17 00:48:48 -0500

    2023_07_15_163306_create_empleados_table.php

--- database/migrations/2023_07_15_163306_create_empleados_table.php
+++ database/migrations/2023_07_15_163306_create_empleados_table.php
@@ -13,6 +13,17 @@ return new class extends Migration
     {
         Schema::create('empleados', function (Blueprint $table) {
             $table->id();
+            $table->string('nombre', 50);
+            $table->string('apellido', 50);
+            $table->string('direccion', 100);
+            $table->string('telefono', 10);
+
+
+
+            $table->bigInteger('ciudad_id')->unsigned();
+
+            $table->foreign('ciudad_id')->references('id')->on('ciudad')->onDelete('cascade');
+
             $table->timestamps();
         });
     }
