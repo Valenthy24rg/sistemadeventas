fa49441ae1a632a1f30eb574459d97734f9b62e4
Valenthy24rg <110358760+Valenthy24rg@users.noreply.github.com>
2023-07-17 00:48:19 -0500

    2023_07_15_163116_create_categorias_table.php

--- database/migrations/2023_07_15_163116_create_categorias_table.php
+++ database/migrations/2023_07_15_163116_create_categorias_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('categorias', function (Blueprint $table) {
             $table->id();
+
+            $table->string('nombre', 50);
+            $table->text('descripcion');
+
+
             $table->timestamps();
         });
     }
